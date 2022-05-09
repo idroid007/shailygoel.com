426182c0d4533fa91f01d7686319ab61d2aefe8d
Ishu Sharma <ishubot@gmail.com>
2022-05-09 18:01:01 +0530

    responsive menu fixed

--- js/base.js
+++ js/base.js
@@ -2,7 +2,10 @@ const button = document.querySelector(".top_menu_contact_me_button");
 button.onclick = () =>{
     window.location.href = "mailto:shailygoel31@gmail.com";
 }
-
+const button2 = document.querySelector(".mobile_menu_contact_me_button");
+button2.onclick = () =>{
+    window.location.href = "mailto:shailygoel31@gmail.com";
+}
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -12,3 +15,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+document.querySelector(".hamburger").onclick = ()=>{
+    const mobile_items = document.querySelector(".mobile-items");
+    if(mobile_items.classList.contains("active")){
+        mobile_items.classList.remove("active");
+        mobile_items.style.left = "-100%";
+    }else{
+        mobile_items.classList.add("active");
+        mobile_items.style.left = "0";
+    }
+}
